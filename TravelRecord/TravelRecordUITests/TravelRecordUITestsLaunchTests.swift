fa24eda0f1c5cc121fac6d86b02159aa8f2da4ce
gyeom0919 <leegyeom09@gmail.com>
2022-12-22 10:13:50 +0900

    ✨ Created Project

---
+++ TravelRecord/TravelRecordUITests/TravelRecordUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TravelRecordUITestsLaunchTests.swift
+//  TravelRecordUITests
+//
+//  Created by 겸 on 2022/12/22.
+//
+
+import XCTest
+
+final class TravelRecordUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
