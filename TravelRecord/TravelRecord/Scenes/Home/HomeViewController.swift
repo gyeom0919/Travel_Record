0a71ab9b2a33e37938194279b5272901ebf6d9f8
gyeom0919 <leegyeom09@gmail.com>
2022-12-23 11:10:50 +0900

    ✨ UITableView Setting

--- TravelRecord/TravelRecord/Scenes/Home/HomeViewController.swift
+++ TravelRecord/TravelRecord/Scenes/Home/HomeViewController.swift
@@ -11,7 +11,8 @@ import Then
 class HomeViewController : BaseViewController {
     
     private let travelTableView = UITableView().then {
-        $0.backgroundColor = .systemBlue
+        $0.backgroundColor = .white
+        
     }
     
     override func addView() {
@@ -19,14 +20,50 @@ class HomeViewController : BaseViewController {
     }
     
     override func configureVC() {
+        travelTableView.delegate = self
+        travelTableView.dataSource = self
+        
+        travelTableView.register(TravelTableViewCell.self, forCellReuseIdentifier: "CustomCell")
         view.backgroundColor = .systemYellow
     }
     
     override func setLayout() {
         travelTableView.snp.makeConstraints{
-            $0.edges.equalToSuperview().offset(20).inset(20)
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(50)
         }
     }
+    
+    override func configureNavigation() {
+        self.navigationItem.largeTitleDisplayMode = .always
+        self.navigationItem.title = "Record your Travel"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "기록 추가", style: .plain, target: self, action: .none)
+    }
+}
+
+//MARK: - UITableSetting
+
+extension HomeViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = travelTableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! TravelTableViewCell
+        
+        cell.textLabel?.text = "Cell at row \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
 }
 
 
---
+++ TravelRecord/TravelRecord/Scenes/Home/View/TravelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelTableViewCell.swift
+//  TravelRecord
+//
+//  Created by 겸 on 2022/12/22.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
